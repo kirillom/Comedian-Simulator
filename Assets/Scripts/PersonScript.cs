09f1b04b33a43c888cabade1f8f55eead0d29df9
Kyrylo Omelianchuk <kirill.omelyanchuk@gmail.com>
2024-01-28 12:48:38 +0200

    commit

--- Assets/Scripts/PersonScript.cs
+++ Assets/Scripts/PersonScript.cs
@@ -4,11 +4,43 @@ using UnityEngine;
 
 public class PersonScript : MonoBehaviour
 {
+    public Material material1;
+    public Material material2;
+    public Material material3;
+    public Material material4;
+    public Material material5;
+    public Material material6;
+    public Material material7;
     public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        int material = Random.Range(1, 7);
+        MeshRenderer mesh = transform.GetChild(0).GetComponent<MeshRenderer>();
+        switch(material)
+        {
+            case 1:
+                mesh.material = material1;
+                break;
+            case 2:
+                mesh.material = material2;
+                break;
+            case 3:
+                mesh.material = material3;
+                break;
+            case 4:
+                mesh.material = material4;
+                break;
+            case 5:
+                mesh.material = material5;
+                break;
+            case 6:
+                mesh.material = material6;
+                break;
+            case 7:
+                mesh.material = material7;
+                break;
+        }
     }
 
     // Update is called once per frame
